dd94d9a60c2a65f1029f0e1d74ac856356dad466
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-04-21 13:50:29 +0530

    final push

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -1,1 +1,14 @@
-let fs=require('./content.md');
+let {readFile,readFileSync}=require('fs');
+
+readFile('./content.md','utf-8',(err,content)=>{
+    console.log(content);
+});
+
+let result=readFileSync('./content.md','utf-8');
+
+console.log(`${result} synchronous`);
+
+let buff1=Buffer.alloc(10);
+
+buff1.write('Welcome to Buffer');
+console.log(buff1);
