dcff76a85ee6ba9a7b43ae538eeee013cef2625f
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-04-23 10:49:18 +0530

    final push

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -2,12 +2,17 @@ var http=require('http');
 
 var server = http.createServer(handleServer);
 function handleServer(req,res){
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.pathname);
-    res.setHeader('Content-type','json');
-    res.end('{message: this is a post request}');
+    if(req.method==='GET' && req.url==='/'){
+        res.setHeader('Content-type','text/plain');
+        res.end(`Welcome to Homepage`);
+    }else if(req.method==='GET'&& req.url ==='/about'){
+        res.setHeader('Content-type','text/html');
+        res.end(`This is all about NodeJs`);
+    }else if(req.method==='POST'&& req.url ==='/about'){
+        res.setHeader('Content-type','application/json');
+        res.end('{message: this is a post request}');
+    }
 }
-server.listen(3500,()=>{
-    console.log(`Sever listening on port 3500`);
+server.listen(3600,()=>{
+    console.log(`Sever listening on port 3600`);
 });
