215c4be977852a23cce469be585531144d279ec7
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-04-23 14:23:29 +0530

    final push

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -1,13 +1,21 @@
-var http=require('http');
-var fs=require('fs');
-var server=http.createServer(handleRequest);
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(handleRequest);
 
-function handleRequest(req,res){
-fs.readFile('./about.html',(err,content)=>{
-    
-})
+function handleRequest(req, res) {
+  if (req.method==='GET'&& req.url === "/file") {
+    fs.readFile("./node.html", (err, content) => {
+        if(err) console.log(err);
+        res.setHeader('Content-type','text/html');
+        res.end(content);
+    });
+  }
+  if(req.method==='GET'&& req.url==="/stream"){
+    res.setHeader('Content-type','text/html');
+    fs.createReadStream('./node.html').pipe(res);
+  }
 }
 
-server.listen(5555,()=>{
-    console.log('Server listening on port 5555')
-})
+server.listen(2100, () => {
+  console.log("Server listening on port 2100");
+});
